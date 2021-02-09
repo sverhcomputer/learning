d2e06721d749a8679140492fe696b500b3d36e7a
sverhcomputer <sverhcomputer@yandex.ru>
2021-02-09 17:29:05 +0300

    Устраняем проблему в неточности подсчётов при конвертации в систему измерения температуры с единицами: Цельсия

--- language_c/fahr_to_cels.c
+++ language_c/fahr_to_cels.c
@@ -4,7 +4,7 @@
     для fahr = 0, 20, ..., 300 */
 
 main() {
-  int fahr, celsius;
+  float fahr, celsius;
   int lower, upper, step;
 
   lower = 0; /* нижняя граница температур в таблице */
@@ -13,6 +13,6 @@ main() {
 
   fahr = lower;
   while (fahr <= upper) {
-    celsius = 5 * (fahr-32) / 9;
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
     fahr = fahr + step;}}
