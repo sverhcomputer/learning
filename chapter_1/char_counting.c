31a44c40ce70fe7c16f41df9f9c5706665691800
sverhcomputer <sverhcomputer@yandex.ru>
2021-02-10 15:11:35 +0300

    Используем for вместо while

--- chapter_1/char_counting.c
+++ chapter_1/char_counting.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-/* подсчёт символов во входном потоке; 1-я версия */
+/* подсчёт символов во входном потоке; 2-я версия */
 
 main() {
-  long nc;
+  double nc;
 
-  nc = 0;
-  while (getchar() != EOF)
-    ++nc;
-  printf("%1d\n", nc);
+  for (nc=0; getchar() != EOF; ++nc);
+  printf("%.0f\n", nc);
 }
