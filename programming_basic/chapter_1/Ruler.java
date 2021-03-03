eded320184d21972cffdd3a094f8c8f87e443d9d
sverhcomputer <sverhcomputer@yandex.ru>
2021-03-03 21:16:27 +0300

    Страница 42. Листинг 1.2.1 Конкатенация строк.

---
+++ programming_basic/chapter_1/Ruler.java
@@ -0,0 +1,25 @@
+public class Ruler {
+    public static void main(String[] args ) {
+    
+        String ruler1 = "1";
+        String ruler2 = ruler1 + "2" + ruler1;
+	String ruler3 = ruler2 + "3" + ruler2;
+	String ruler4 = ruler3 + "4" + ruler3;
+	String ruler5 = ruler4 + "5" + ruler4;
+	String ruler6 = ruler5 + "6" + ruler5;
+	String ruler7 = ruler6 + "7" + ruler6;
+	System.out.println(ruler1);
+	System.out.println(ruler2);
+	System.out.println(ruler3);
+	System.out.println(ruler4);
+	System.out.println(ruler5);
+	System.out.println(ruler6);
+	System.out.println(ruler7);
+	System.out.println(ruler6);
+	System.out.println(ruler5);
+	System.out.println(ruler4);
+	System.out.println(ruler3);
+	System.out.println(ruler2);
+	System.out.println(ruler1); }}
+	
+    
