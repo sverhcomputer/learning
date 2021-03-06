eb06e9ca3c1ddf89059a78618d1de503567639dc
sverhcomputer <sverhcomputer@yandex.ru>
2021-03-06 19:39:21 +0300

    страница 75. Преобразование типов.

---
+++ oopcpp/chapter2/mixed.cpp
@@ -0,0 +1,10 @@
+// использование смешанных выражений
+#include <iostream>
+using namespace std;
+int main() {
+    int count = 7;
+    float avgWeight = 155.5F;
+    double totalWeight = count *avgWeight;
+    cout << "Вес равен: " << totalWeight << endl;
+    return 0;
+}
